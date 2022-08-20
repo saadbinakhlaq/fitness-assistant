286894781e6f75a39d73a099ec9aeb29251f610d
Saad <saad.18@gmail.com>
2022-08-20 12:21:48 +0200

    Initial commit

---
+++ fitness-assistant-p1-m2/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function handleRunTraining() {
+  console.log('Run training');
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={handleRunTraining}>Run Training</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
